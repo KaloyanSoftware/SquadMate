a062f4822245815a6d2b31a9b8211b67881614ba
Kaloyan <kaloyan.ivanov@student.kdg.be>
2025-06-21 18:18:04 +0300

    Initial commit

---
+++ src/main/java/com/portfolio/squadmate/SquadMateApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.squadmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquadMateApplication.class, args);
+    }
+
+}
